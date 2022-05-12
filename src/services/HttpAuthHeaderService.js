eed92c2a44d5c9e2f8025dd242dd71b7f05bd1bf
S1mpleKnight <become.adult77@gmail.com>
2022-05-12 16:26:43 +0300

    feat: add edit profile logic

--- src/services/HttpAuthHeaderService.js
+++ src/services/HttpAuthHeaderService.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import authHeader from "./TokenHeaderService";
 
+const BASE_URL = "http://localhost:8080/api"
+
 export default axios.create({
-    baseURL: "http://localhost:80/api",
+    baseURL: BASE_URL,
     headers: {
         "Content-type": "application/json",
         "Authorization": authHeader(),
