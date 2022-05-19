c7a2fddf395db3183a520c129b13ada2c240e2b9
S1mpleKnight <become.adult77@gmail.com>
2022-05-19 12:37:00 +0300

    new: add heroku backend url

--- src/services/HttpAuthHeaderService.js
+++ src/services/HttpAuthHeaderService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import authHeader from "./TokenHeaderService";
 
-const BASE_URL = "http://localhost:8080/api"
+// const BASE_URL = "http://localhost:8080/api"
+const BASE_URL = "https://questionnaire-portal-system.herokuapp.com/api"
 
 export default axios.create({
     baseURL: BASE_URL,
