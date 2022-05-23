efd55443e81a308920ea909ecabc6204da40aae6
S1mpleKnight <become.adult77@gmail.com>
2022-05-23 17:07:02 +0300

    feat: add questionnaire logic

--- src/pages/Congratulation/Congratulation.js
+++ src/pages/Congratulation/Congratulation.js
@@ -1,10 +1,11 @@
 import {NavBar} from "../../components/NavBar/NavBar";
+import AuthService from "../../services/AuthService";
 
 function Congratulation() {
     return (
         <>
             <div className="bg-light" style={{height: '100vh'}}>
-                <NavBar auth={false}/>
+                <NavBar auth={AuthService.getCurrentUser()}/>
                 <div className="bg-white mt-4 border m-auto p-4" style={{width: '30vw'}}>
                     <h3 className="text-center mb-3">Thank you!</h3>
                     <p className="text-center">Your response was saved.</p>
--- src/pages/Questionnaire/Questionnaire.js
+++ src/pages/Questionnaire/Questionnaire.js
@@ -3,19 +3,20 @@ import {Button, Container, Form} from "react-bootstrap";
 import React, {Component} from "react";
 import AuthService from "../../services/AuthService";
 import QuestionnaireService from "../../services/QuestionnaireService";
-import {useParams} from "react-router-dom";
+import {Navigate, useParams} from "react-router-dom";
 
 const OPTIONS_DELIMITER = "~!@#%&_&%#@!~";
 
 class Questionnaire extends Component {
     constructor(props) {
-        console.log(props)
         super(props);
         this.createAnswerField = this.createAnswerField.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
         this.state = {
             fields: [],
             answers: [],
             message: "",
+            redirect: "",
             param: props.params.questionnaireId.toString()
         }
     }
@@ -34,23 +35,23 @@ class Questionnaire extends Component {
         switch (field.fieldType) {
             case "DATE":
             case "SINGLE_LINE_TEXT":
-                controlElement = (<Form.Control className="text-dark" type="text"/>)
+                controlElement = (<Form.Control name={field.label} className="text-dark" type="text"/>)
                 break
             case "MULTILINE_TEXT":
-                controlElement = (<Form.Control className="text-dark" type="textarea"/>)
+                controlElement = (<Form.Control name={field.label} className="text-dark" type="textarea"/>)
                 break
             case "RADIO_BUTTON":
                 controlElement = field.fieldOptions.replaceAll(OPTIONS_DELIMITER, " ").split(" ")
                     .map((option) => {
-                        return (<Form.Check type="radio">{option}</Form.Check>)
+                        return (<Form.Check name={field.label} type="radio">{option}</Form.Check>)
                     })
                 break
             case "CHECKBOX":
-                controlElement = (<Form.Check type="checkbox"/>)
+                controlElement = (<Form.Check name={field.label} type="checkbox"/>)
                 break
             case "COMBOBOX":
                 controlElement = (
-                    <Form.Select>
+                    <Form.Select name={field.label} multiple>
                         {
                             field.fieldOptions.replaceAll(OPTIONS_DELIMITER, " ").split(" ")
                                 .map((option) => {
@@ -70,8 +71,93 @@ class Questionnaire extends Component {
     }
 
     handleSubmit(e) {
-        e.preventDefault()
+        this.setState({
+            message: ""
+        })
 
+        const formData = new FormData(e.target);
+        e.preventDefault();
+        for (let [key, value] of formData.entries()) {
+            this.state.answers.push({
+                "label" : key,
+                "value" : value
+            })
+        }
+
+        const result = []
+        for (let i = 0; i < this.state.fields.length; i++) {
+            if (!this.state.fields[i].active) {
+                result.push({
+                    position: (i+1),
+                    value: "N/A"
+                })
+                continue
+            }
+            if (this.state.fields[i].required) {
+                const value = this.state.answers.filter((answer) => answer.label === this.state.fields[i].label)
+
+                if (this.state.fields[i].fieldType === "CHECKBOX") {
+                    if (value.length === 0) {
+                        result.push({
+                            position: (i+1),
+                            value: false
+                        })
+                    } else {
+                        result.push({
+                            position: (i+1),
+                            value: true
+                        })
+                    }
+                    continue
+                }
+
+                if (value.length === 0) {
+                    this.setState({
+                        message: `Required answer for the ${this.state.fields[i].label} field`
+                    })
+                    return
+                } else if (value.length > 0 && this.state.fields[i].fieldType === "COMBOBOX") {
+                    result.push({
+                        position: (i+1),
+                        value: value.map((v) => v.value).join(", ")
+                    })
+                } else {
+                    result.push({
+                        position: (i+1),
+                        value: value[0].value
+                    })
+                }
+            } else {
+                const value = this.state.answers.filter((answer) => answer.label === this.state.fields[i].label)
+                if (value.length === 0) {
+                    result.push({
+                        position: (i+1),
+                        value: "N/A"
+                    })
+                } else if (value.length > 0 && this.state.fields[i].fieldType === "COMBOBOX") {
+                    result.push({
+                        position: (i+1),
+                        value: value.map((v) => v.value).join(", ")
+                    })
+                }else {
+                    result.push({
+                        position: (i+1),
+                        value: value[0].value
+                    })
+                }
+            }
+        }
+        QuestionnaireService.postResponses(this.state.param, result)
+            .then(
+                (r) => {
+                    this.setState({
+                        redirect: true
+                    })
+                },
+                error => {
+                    this.setState({message: error.response.data})
+                }
+            )
 
     }
 
@@ -79,7 +165,6 @@ class Questionnaire extends Component {
         QuestionnaireService.getQuestionnaire(this.state.param)
             .then(
                 (r) => {
-                    console.log(r)
                     this.setState({
                         fields: r.data,
                     })
@@ -91,22 +176,25 @@ class Questionnaire extends Component {
     }
 
     render() {
+        if (this.state.redirect) {
+            return <Navigate to="/success"/>
+        }
         return (
             <>
                 <div className="bg-light" style={{height: '100vh'}}>
                     <NavBar auth={AuthService.getCurrentUser()}/>
                     <Container style={{width: '35vw'}} className="bg-white border mt-4 p-0">
-                        <Form noValidate className="px-5 py-4">
+                        <Form noValidate className="px-5 py-4" onSubmit={this.handleSubmit}>
                             {this.state.fields.map((field) => this.createAnswerField(field))}
                             <Button className="w-25" variant="primary" type="submit">
                                 SUBMIT
                             </Button>
+                            {this.state.message && (
+                                <div className="mt-3 alert alert-danger" role="alert">
+                                    {this.state.message}
+                                </div>
+                            )}
                         </Form>
-                        {this.state.message && (
-                            <div className="alert alert-danger" role="alert">
-                                {this.state.message}
-                            </div>
-                        )}
                     </Container>
                 </div>
             </>
